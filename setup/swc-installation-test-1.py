8574f41adf59af34938e1bcfd0990367ccca4590
Thomas Kluyver <takowl@gmail.com>
2015-06-03 15:56:52 -0700

    Check for Python 3

--- setup/swc-installation-test-1.py
+++ setup/swc-installation-test-1.py
@@ -39,7 +39,7 @@ __version__ = '0.1'
 
 
 def check():
-    if _sys.version_info < (2, 6):
+    if _sys.version_info < (3, 3):
         print('check for Python version (python):')
         print('outdated version of Python: ' + _sys.version)
         return False
@@ -51,6 +51,6 @@ if __name__ == '__main__':
         print('Passed')
     else:
         print('Failed')
-        print('Install a current version of Python!')
-        print('http://www.python.org/download/releases/2.7.3/#download')
+        print('Install a current version of Python 3!')
+        print('https://www.python.org/downloads/')
         _sys.exit(1)
